583d553571df9f900a1a5b15cf8cb2b3ca2c8880
Gonzalo de Nicolás <g.nicolas@smdos.es>
2018-10-11 10:24:27 +0200

    1.1.1
    >Asociate añadido.

---
+++ src/paginas/asociate.php
@@ -0,0 +1,37 @@
+<?php include '../utils/cabecera.html'?>
+
+<!-- Promo Block -->
+<section class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall " data-options='{direction: "fromtop", animation_duration: 25, direction: "reverse"}'>
+  <!-- Parallax Image -->
+  <div class="divimage dzsparallaxer--target w-100 u-bg-overlay g-bg-black-opacity-0_3--after" style="height: 100%; background-image: url(../../assets/img/asociate.jpg);"></div>
+  <!-- End Parallax Image -->
+
+  <!-- Promo Block Content -->
+  <div class="container g-color-white text-center g-py-120">
+    <h2 class="h1 g-font-weight-600 text-uppercase mb-2" id="borde_letra">Asóciate</h2>
+    <!-- <p class="g-font-weight-300 g-font-size-22 text-uppercase" id="borde_letra">Curso de oratoria para profesores</p> -->
+  </div>
+  <!-- Promo Block Content -->
+</section>
+<!-- End Promo Block -->
+
+<!-- Portfolio Case Study -->
+<section class="container g-pt-100 g-pb-70">
+  <div class="row">
+    <div class="col-md-offset-2 col-md-8 g-mb-30">
+      <p>Te ofrecemos e invitamos a formar parte de nuestro colectivo como asociado.<br/>
+        ser socio de <span class="g-color-primary">APRORA</span>, te permite participar activamente en las actividades de la Asociación:</p>
+        <ul class="list-group list-group-flush g-pt-20">
+          <li class="list-group-item">Charlas y conferencia magistrales por especialistas en Oratoria</li>
+          <li class="list-group-item">Descuento en inscripción de concursos</li>
+          <li class="list-group-item">Asistencia al encuentro anual de socios</li>
+          <li class="list-group-item">Ayudarás a un buen fin : propagar la oratoria en centros educativos</li>
+        </ul>
+        <p class="g-pt-40">Y un largo conjunto de posibilidades que favorecerán tu desarrollo en oratoria a nivel individual, colectivo y social.<br/>
+          ¡Hazte socio por sólo 30 €/año¡</p>
+          <p>Existe también  opción de inscripción a nivel colectivo: Centro educativo, Empresas, Organizaciones ( consultar)<br/>para hacerte socio/a de APRORA sólo tienes que rellenar el <a href="../../resources/hoja_inscripcion.doc">formulario de inscripción</a> y enviarlo por email a: <a href="mailto:administracion@aprora.com">administración@aprora.com</a>.</p>
+        </div>
+      </div>
+    </section>
+    <!-- End Portfolio Case Study -->
+    <?php include '../utils/pie.html'?>
