2481e01cfc42abb41d625ff43cb3d37750a88476
gondeni <gondeni.romero@gmail.com>
2019-10-17 17:28:12 +0200

    Pruebas galería
    Ordenacion de librerias y dependencias scss css js

--- src/paginas/en_los_medios.php
+++ src/paginas/en_los_medios.php
@@ -1,38 +1,48 @@
-<?php  include '../utils/cabecera.html'; ?>
+<?php include '../utils/cabecera.html'; ?>
 
 <!-- Promo Block -->
-<section class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall " data-options='{direction: "fromtop", animation_duration: 25, direction: "reverse"}'>
-  <!-- Parallax Image -->
-  <div class="divimage dzsparallaxer--target w-100 u-bg-overlay g-bg-black-opacity-0_3--after" style="height: 120%; background-image: url(../../assets/img/img10.jpeg);"></div>
-  <!-- End Parallax Image -->
+<section class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall "
+         data-options='{direction: "fromtop", animation_duration: 25, direction: "reverse"}'>
+    <!-- Parallax Image -->
+    <div class="divimage dzsparallaxer--target w-100 u-bg-overlay g-bg-black-opacity-0_3--after"
+         style="height: 120%; background-image: url(../../assets/img/img10.jpeg);"></div>
+    <!-- End Parallax Image -->
 
-  <!-- Promo Block Content -->
-  <div class="container g-color-white text-center g-py-120">
-    <h2 class="h1 g-font-weight-600 text-uppercase mb-2" id="borde_letra">APRORA en los medios</h2>
-    <!-- <p class="g-font-weight-300 g-font-size-22 text-uppercase" id="borde_letra">Curso de oratoria para profesores</p> -->
-  </div>
-  <!-- Promo Block Content -->
+    <!-- Promo Block Content -->
+    <div class="container g-color-white text-center g-py-120">
+        <h2 class="h1 g-font-weight-600 text-uppercase mb-2" id="borde_letra">APRORA en los medios</h2>
+        <!-- <p class="g-font-weight-300 g-font-size-22 text-uppercase" id="borde_letra">Curso de oratoria para profesores</p> -->
+    </div>
+    <!-- Promo Block Content -->
 </section>
 <!-- End Promo Block -->
 
 <!-- Portfolio Case Study -->
 <section class="g-bg-gray-light-v5">
 
-  <div class="container g-py-100">
-    <div class="row justify-content-center g-mb-110">
-      <div class="col-md-offset-2 col-md-4">
-        <div class="fb-page" data-href="https://www.facebook.com/Aprora-439520733245941/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Aprora-439520733245941/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Aprora-439520733245941/">Aprora</a></blockquote></div>
-      </div>
-    </div>
-    <div class="row justify-content-center g-mb-110">
-      <div class="col-lg-8">
-        <!-- Heading -->
-        <div class="text-center">
-          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D2170110276352485%26id%3D1135747149788808&width=500" width="100%" height="605" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    <div class="container g-py-100">
+        <div class="row justify-content-center g-mb-110">
+            <div class="col-md-offset-2 col-md-6">
+                <div class="fb-page" data-href="https://www.facebook.com/Aprora-439520733245941/" data-tabs="timeline"
+                     data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true"
+                     data-hide-cover="false" data-show-facepile="true">
+                    <blockquote cite="https://www.facebook.com/Aprora-439520733245941/" class="fb-xfbml-parse-ignore"><a
+                                href="https://www.facebook.com/Aprora-439520733245941/">Aprora</a></blockquote>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="row justify-content-center g-mb-110">
+            <div class="col-lg-8">
+                <!-- Heading -->
+                <div class="text-center">
+                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D2170110276352485%26id%3D1135747149788808&width=500"
+                            width="100%" height="605" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                            allowTransparency="true" allow="encrypted-media"></iframe>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </section>
 
-<?php  include '../utils/pie.html'; ?>
+
+<?php include '../utils/pie.html'; ?>
