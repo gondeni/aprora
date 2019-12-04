b28cded3498a522cfd54bd05db2689735f3912b9
gondeni <gondeni.romero@gmail.com>
2019-12-04 20:53:27 +0100

    Sección "Curso para adultos" subida

---
+++ src/paginas/oratoria_adultos.php
@@ -0,0 +1,89 @@
+<?php include('../utils/cabecera.html') ?>
+<!-- Info Blocks -->
+
+<!-- Promo Block -->
+<!--<section class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall " data-options='{direction: "fromtop", animation_duration: 25, direction: "reverse"}'>-->
+    <!-- Parallax Image -->
+<!--    <div class="divimage dzsparallaxer--target w-100 u-bg-overlay g-bg-black-opacity-0_3--after" style="height: 120%; background-image: url(../../assets/img/img3.jpg);"></div>-->
+    <!-- End Parallax Image -->
+
+    <!-- Promo Block Content -->
+<!--    <div class="container g-color-white text-center g-py-120">-->
+<!--        <h2 class="h1 g-font-weight-600 text-uppercase mb-2" id="borde_letra">Formación</h2>-->
+<!--        <p class="g-font-weight-300 g-font-size-22 text-uppercase" id="borde_letra">Curso de oratoria para profesionales</p>-->
+<!--    </div>-->
+    <!-- Promo Block Content -->
+<!--</section>-->
+<!-- End Promo Block -->
+
+<div class="row no-gutters">
+    <!-- Info Image -->
+    <div class="col-lg-5 g-min-height-300 g-bg-size-cover g-bg-pos-right" data-bg-img-src="../../assets/img/curso_adultos_1.jpeg"></div>
+    <!-- End Info Image -->
+
+    <div class="col-lg-7 g-bg-gray-dark-v1 g-pt-50 g-pb-50 g-px-80">
+        <header class="text-uppercase g-mb-35">
+            <div class="g-mb-30">
+                <h2 class="h2 g-color-white g-font-weight-700 mb-0">Concurso de oratoria para adultos</h2>
+                <span class="d-block g-color-primary g-font-weight-700 g-font-size-default g-mb-15">¡Plazas limitadas!</span>
+            </div>
+            <div class="g-width-70 g-brd-bottom g-brd-2 g-brd-primary"></div>
+        </header>
+
+        <p class="g-color-white-opacity-0_7 g-mb-65"></p>
+
+        <div class="row align-items-stretch">
+            <div class="col-sm-6 g-mb-30">
+                <!-- Article -->
+                <article class="h-100 g-flex-middle g-brd-left g-brd-3 g-brd-primary g-brd-white--hover g-bg-black-opacity-0_8 g-transition-0_3 g-pa-20">
+                    <div class="g-flex-middle-item">
+                        <h4 class="h6 g-color-white g-font-weight-600 text-uppercase g-mb-10">Mejora el uso de tu voz</h4>
+                        <p class="g-color-white-opacity-0_7 mb-0">Seduce con tus discursos y emociona a quienes te escuchan.</p>
+                    </div>
+                </article>
+                <!-- End Article -->
+            </div>
+
+            <div class="col-sm-6 g-mb-30">
+                <!-- Article -->
+                <article class="h-100 g-flex-middle g-brd-left g-brd-3 g-brd-primary g-brd-white--hover g-bg-black-opacity-0_8 g-transition-0_3 g-pa-20">
+                    <div class="g-flex-middle-item">
+                        <h4 class="h6 g-color-white g-font-weight-600 text-uppercase g-mb-10">Aprende a usar tu cuerpo</h4>
+                        <p class="g-color-white-opacity-0_7 mb-0">Al hablar ante un auditorio, ¿Sonries y miras al público?. Consigue el éxito con una presentación impactante.</p>
+                    </div>
+                </article>
+                <!-- End Article -->
+            </div>
+        </div>
+
+        <div class="row align-items-stretch">
+            <div class="col-sm-6 g-mb-30">
+                <!-- Article -->
+                <article class="h-100 g-flex-middle g-brd-left g-brd-3 g-brd-primary g-brd-white--hover g-bg-black-opacity-0_8 g-transition-0_3 g-pa-20">
+                    <div class="g-flex-middle-item">
+                        <h4 class="h6 g-color-white g-font-weight-600 text-uppercase g-mb-10">Lugar:</h4>
+                        <p class="g-color-white-opacity-0_7 mb-0">Colegio "Sagrado Corazón de Jesús".</p>
+                    </div>
+                </article>
+                <!-- End Article -->
+            </div>
+
+            <div class="col-sm-6 g-mb-30">
+                <!-- Article -->
+                <article class="h-100 g-flex-middle g-brd-left g-brd-3 g-brd-primary g-brd-white--hover g-bg-black-opacity-0_8 g-transition-0_3 g-pa-20">
+                    <div class="g-flex-middle-item">
+                        <h4 class="h6 g-color-white g-font-weight-600 text-uppercase g-mb-10">Precio:</h4>
+                        <p class="g-color-white-opacity-0_7 mb-0">150€</p>
+                        <p class="g-color-white-opacity-0_7 mb-0">90€ a Asociados APA Esclavas.</p>
+                    </div>
+                </article>
+                <!-- End Article -->
+            </div>
+        </div>
+
+        <span class="d-block g-color-primary g-font-weight-700 g-font-size-default g-mb-15">Envía un mail a: <a href = "mailto: concursoratoria@aprora.com">concursoratoria@aprora.com</a></span>
+    </div>
+</div>
+<!-- End Info Blocks -->
+
+<?php include('../utils/pie.html') ?>
