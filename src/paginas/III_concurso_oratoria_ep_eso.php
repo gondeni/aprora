f4133491a489990244166c67de52f14b97b64db9
gondeni <gondeni.romero@gmail.com>
2020-01-21 12:58:51 +0100

    3er concurso de oratoria Extremadura.

---
+++ src/paginas/III_concurso_oratoria_ep_eso.php
@@ -0,0 +1,157 @@
+<?php include '../utils/cabecera.html'; ?>
+
+<!-- Promo Block -->
+<section class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall " data-options='{direction: "fromtop", animation_duration: 25, direction: "reverse"}'>
+  <!-- Parallax Image -->
+  <div class="divimage dzsparallaxer--target w-100 u-bg-overlay g-bg-black-opacity-0_3--after" style="height: 120%; background-image: url(../../assets/img/img11.jpg);"></div>
+  <!-- End Parallax Image -->
+
+  <!-- Promo Block Content -->
+  <div class="container g-color-white text-center g-py-120">
+    <h2 class="h1 g-font-weight-600 text-uppercase mb-2" id="borde_letra">Concurso ESO y Bachillerato</h2>
+    <!-- <p class="g-font-weight-300 g-font-size-22 text-uppercase" id="borde_letra">Curso de oratoria para profesores</p> -->
+  </div>
+  <!-- Promo Block Content -->
+</section>
+
+<!-- End Promo Block -->
+<!-- <section class="container g-pt-100 g-pb-40 g-pl-150" >
+  <div class="row">
+    <div class=" col-md-12 g-pt-25 g-pl-50">
+      <img class="img-fluid u-shadow-v2 g-rounded-3 img-responsive" style="max-width:100%;" src="../../assets/img/concurso_oratoria_ep_eso_1.jpg" alt="concurso_oratoria_ep_eso_1">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12 g-pt-25 g-pl-50">
+      <img class="img-fluid u-shadow-v2 g-rounded-3 img-responsive" style="max-width:100%;" src="../../assets/img/concurso_oratoria_ep_eso_2.jpg" alt="concurso_oratoria_ep_eso_2">
+    </div>
+  </div>
+</section> -->
+<!-- Portfolio Case Study -->
+<section class="container g-pt-50 g-pb-70">
+  <div class="u-heading-v2-1 g-brd-primary g-mb-30">
+    <h2 class="text-uppercase u-heading-v2__title g-font-weight-700 mb-0">III Concurso de oratoria E.P. y E.S.O. </h2>
+    <h4 class="g-font-weight-200">Barcarrota (Badajoz), Extremadura</h4>
+  </div>
+  <div class="row">
+    <!-- <div class="col-md-6">
+      <img class="img-fluid u-shadow-v2 g-rounded-3 img-responsive" style="max-width:100%;" src="../../assets/img/logos/consejeria.png" alt="Consejería de educación y empleo">
+    </div>
+    <div class="col-md-4 g-pt-25 g-pl-50">
+      <img class="img-fluid u-shadow-v2 g-rounded-3 img-responsive" style="max-width:100%;" src="../../assets/img/logos/hoy.jpg" alt="Diario 'HOY'">
+    </div> -->
+    <div class=" col-md-12 g-pt-25 g-pl-50">
+      <a href="../../assets/img/III_concurso_oratoria.jpg" class="g-color-primary" target="_blank"><img class="img-fluid u-shadow-v2 g-rounded-3 img-responsive" style="max-width:100%;" src="../../assets/img/concurso_oratoria_ep_eso_1.jpg" alt="concurso_oratoria_ep_eso_1"></a>
+    </div>
+  </div>
+<!--  <div class="row">
+    <div class="col-md-12 g-pt-25 g-pl-50">
+      <a href="../../resources/concurso_oratoria_ep_eso_2.jpg" class="g-color-primary" target="_blank"><img class="img-fluid u-shadow-v2 g-rounded-3 img-responsive" style="max-width:100%;" src="../../assets/img/concurso_oratoria_ep_eso_2.jpg" alt="concurso_oratoria_ep_eso_2"></a>
+    </div>-->
+  </div>
+
+  <!-- <div class="shortcode-html">
+    <div id="accordion-07" class="u-accordion u-accordion-color-primary" role="tablist" aria-multiselectable="true">
+      <div class="card rounded-0 g-brd-none">
+        <div id="accordion-07-heading-01" class="u-accordion__header g-pa-0" role="tab">
+          <h5 class="mb-0 text-uppercase g-font-size-default g-font-weight-700 g-pa-20a mb-0">
+            <a class="d-block g-color-main g-text-underline--none--hover" href="#accordion-07-body-01" data-toggle="collapse" data-parent="#accordion-07" aria-expanded="true" aria-controls="accordion-07-body-01">
+              <span class="u-accordion__control-icon d-inline-block g-brd-right g-brd-gray-light-v4 g-color-primary text-center g-pa-20">
+                <i class="fa fa-plus"></i>
+                <i class="fa fa-minus"></i>
+              </span>
+              <span class="d-inline-block g-pa-15">
+                ¿Por qué?
+              </span>
+            </a>
+          </h5>
+        </div>
+        <div id="accordion-07-body-01" class="collapse show" role="tabpanel" aria-labelledby="accordion-07-heading-01">
+          <div class="u-accordion__body g-bg-gray-light-v5 g-px-50 g-py-30"><p>
+            Comunicar es el acto social humano por excelencia. Durante mucho tiempo el sistema
+            educativo español ha considerado que no era necesario su aprendizaje, sino que por el
+            hecho de tener unas ideas ya sabíamos transmitirlas. En la actualidad nuestra sociedad
+            va tomando poco a poco conciencia de la necesidad de formarnos en las habilidades
+            comunicativas. Como todo “arte”, hablar en público tiene sus técnicas y necesitamos
+            aprenderlas y practicarlas. Cada día se hace más necesaria la formación en técnicas para
+            la retórica, la dialéctica y la elocuencia.<br/><br/>
+            Según estudios recientes el 80% de las profesiones futuras requerirán hablar en público.
+            Si desde las instituciones se exige a los alumnos un aprendizaje en competencias, siendo
+            “aquellas que todas las personas precisan para su realización y desarrollo personal, así
+            como para la ciudadanía activa, la inclusión social y el empleo”, entendemos como
+            idóneo, necesario y prioritario la adquisición de las habilidades comunicativas.<br/>
+            Tras el éxito del primer Concurso de Oratoria celebrado el año pasado dirigido a
+            alumnos de Educación Primaria, queremos incidir y satisfacer con esta propuesta los
+            fines y objetivos que, en el ámbito de la comunicación y la expresión oral, exigen la
+            LOMCE y las Disposiciones Autonómicas. Por ello ampliamos el concurso a estudiantes
+            de ESO y Bachillerato.
+        </div>
+      </div>
+    </div>
+
+    <div class="card rounded-0 g-brd-none">
+      <div id="accordion-07-heading-02" class="u-accordion__header g-pa-0" role="tab">
+        <h5 class="mb-0 text-uppercase g-font-size-default g-font-weight-700 g-pa-20a mb-0">
+          <a class="collapsed d-block g-color-main g-text-underline--none--hover" href="#accordion-07-body-02" data-toggle="collapse" data-parent="#accordion-07" aria-expanded="false" aria-controls="accordion-07-body-02">
+            <span class="u-accordion__control-icon d-inline-block g-brd-right g-brd-gray-light-v4 g-color-primary text-center g-pa-20">
+              <i class="fa fa-plus"></i>
+              <i class="fa fa-minus"></i>
+            </span>
+            <span class="d-inline-block g-pa-15">
+              Convocatoria
+            </span>
+          </a>
+        </h5>
+      </div>
+      <div id="accordion-07-body-02" class="collapse" role="tabpanel" aria-labelledby="accordion-07-heading-02">
+        <div class="u-accordion__body g-bg-gray-light-v5 g-px-50 g-py-30">
+          <p>
+            APRORA, sin ánimo de lucro, en colaboración con el Ayuntamiento de Barcarrota y el Diario HOY convoca el II Concurso de Oratoria, diseñado y abierto a la participación de los alumnos de 5º y 6º de Educación Primaria y 1º y 2º de la E.S.O. de todos los Centros Educativos de Extremadura, formándose con ellos DOS CATEGORÍAS:<br/><br/> Categoría 1, alumnos de 5º y 6º de E.P. <br/><br/> Categoría 2, alumnos de 1º y 2º de E.S.O., pudiéndose combinar en cada Categoría alumnos de ambos cursos.
+          </p>
+        </div>
+      </div>
+    </div>
+
+   <div class="card rounded-0 g-brd-none">
+      <div id="accordion-07-heading-03" class="u-accordion__header g-pa-0" role="tab">
+        <h5 class="mb-0 text-uppercase g-font-size-default g-font-weight-700 g-pa-20a mb-0">
+          <a class="collapsed d-block g-color-main g-text-underline--none--hover" href="#accordion-07-body-03" data-toggle="collapse" data-parent="#accordion-07" aria-expanded="false" aria-controls="accordion-07-body-03">
+            <span class="u-accordion__control-icon d-inline-block g-brd-right g-brd-gray-light-v4 g-color-primary text-center g-pa-20">
+              <i class="fa fa-plus"></i>
+              <i class="fa fa-minus"></i>
+            </span>
+            <span class="d-inline-block g-pa-15">
+              Cronograma
+            </span>
+          </a>
+        </h5>
+      </div>
+      <div id="accordion-07-body-03" class="collapse" role="tabpanel" aria-labelledby="accordion-07-heading-03">
+        <div class="u-accordion__body g-bg-gray-light-v5 g-px-50 g-py-30">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius congue quam, vitae condimentum turpis rhoncus id. Cras non mauris eros. Vestibulum convallis ac felis tristique condimentum. Nunc accumsan erat sed justo molestie interdum. Vivamus iaculis volutpat quam, sed porttitor sem maximus vel. Cras dictum vel nibh sit amet iaculis. Maecenas ut massa at velit semper lacinia. Ut non eros ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sit amet commodo nisl. Duis fermentum molestie odio, vitae pulvinar lacus faucibus a. Ut non ornare diam. Quisque ut eros laoreet, interdum metus sit amet, pretium erat. Pellentesque ac consequat elit.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+
+<!-- Bases e inscripción -->
+<div class="row g-pt-50">
+  <p class="lead">Más información e inscripción: </p><br/>
+</div>
+<div class="row g-pt-10">
+  <a href="../../resources/concurso_oratoria_2020.pdf" class="g-color-primary" target="_blank">Bases</a>
+</div>
+<!-- <div class="row g-pt-10">
+  <a href="../../resources/concurso_oratoria_ep_eso_2.jpg" class="g-color-primary" target="_blank">Imagen 2</a>
+</div> -->
+<div class="row g-pt-10">
+  <a href="../../resources/hoja_inscripcion.doc" class="g-color-primary" target="_blank">Hoja de inscripción</a>
+</div>
+
+</section>
+<!-- End Portfolio Case Study -->
+<?php include '../utils/pie.html'; ?>
